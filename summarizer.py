aeb4d058a2fc879b250fed34e571f70cf06f0aef
klintchristofferNSA <klintchristoffer1@gmail.com>
2025-12-17 14:29:37 +0100

    summarizer.py

---
+++ summarizer.py
@@ -0,0 +1,6 @@
+import re
+
+def summarize_text(text, max_sentences=10
+    sentences = re.split(r'(?<=[.!?]) +', text)
+    sentences = [s.strip() for s in sentences if len(s) > 40]
+    return " ".join(sentences[:max_sentences])
